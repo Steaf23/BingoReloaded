3a021f51b3473d18fad250f2e1c43884e418c84d
chrisjanusa <chrisjanusa@gmail.com>
2023-06-13 18:34:00 -0700

    Refactor Effects Menu into an Action Menu

--- BingoReloaded/src/main/java/io/github/steaf23/bingoreloaded/gui/EffectOptionsMenu.java
+++ BingoReloaded/src/main/java/io/github/steaf23/bingoreloaded/gui/EffectOptionsMenu.java
@@ -1,6 +1,7 @@
 package io.github.steaf23.bingoreloaded.gui;
 
 import io.github.steaf23.bingoreloaded.gameloop.BingoSession;
+import io.github.steaf23.bingoreloaded.gui.base.ActionMenu;
 import io.github.steaf23.bingoreloaded.settings.BingoSettingsBuilder;
 import io.github.steaf23.bingoreloaded.data.BingoTranslation;
 import io.github.steaf23.bingoreloaded.gui.base.MenuItem;
@@ -16,70 +17,52 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class EffectOptionsMenu extends MenuInventory {
-    private final BingoSettingsBuilder settings;
+public class EffectOptionsMenu extends ActionMenu {
     private final EnumSet<EffectOptionFlags> flags;
-    private final EffectOptionsMenuItem[] menuItems;
-
-    private final BingoSession session;
 
     public EffectOptionsMenu(MenuInventory parent, BingoSettingsBuilder settings, BingoSession session) {
         super(45, BingoTranslation.OPTIONS_EFFECTS.translate(), parent);
-        this.settings = settings;
-        this.session = session;
-
-        menuItems = new EffectOptionsMenuItem[]{
-                new EffectOptionsMenuItem(EffectOptionFlags.NIGHT_VISION, 4, 3, Material.CARROT),
-                new EffectOptionsMenuItem(EffectOptionFlags.WATER_BREATHING, 2, 3, Material.PUFFERFISH),
-                new EffectOptionsMenuItem(EffectOptionFlags.FIRE_RESISTANCE, 6, 3, Material.MAGMA_CREAM),
-                new EffectOptionsMenuItem(EffectOptionFlags.NO_FALL_DAMAGE, 2, 1, Material.NETHERITE_BOOTS),
-                new EffectOptionsMenuItem(EffectOptionFlags.SPEED, 4, 1, Material.FEATHER),
-                new EffectOptionsMenuItem(EffectOptionFlags.NO_DURABILITY, 6, 1, Material.NETHERITE_PICKAXE),
-                new EffectOptionsMenuItem(
-                        null,
-                        44, Material.DIAMOND,
-                        "" + ChatColor.AQUA + ChatColor.BOLD + BingoTranslation.MENU_SAVE_EXIT.translate()
-                )
-        };
-        MenuItem[] options = Stream.of(menuItems).map(EffectOptionsMenuItem::getMenuItem).toArray(MenuItem[]::new);
-        addItems(options);
-
         flags = settings.view().effects();
-        for (EffectOptionsMenuItem menuItem : menuItems) {
-            updateUI(menuItem);
-        }
-    }
 
-    @Override
-    public void onItemClicked(InventoryClickEvent event, int slotClicked, Player player, ClickType clickType) {
-        for (EffectOptionsMenuItem menuItem : menuItems) {
-            if (slotClicked == menuItem.getMenuItem().getSlot()) {
-                if (menuItem.getFlag() == null) {
+        addEffectAction(EffectOptionFlags.NIGHT_VISION, 4, 3, Material.CARROT);
+        addEffectAction(EffectOptionFlags.WATER_BREATHING, 2, 3, Material.PUFFERFISH);
+        addEffectAction(EffectOptionFlags.FIRE_RESISTANCE, 6, 3, Material.MAGMA_CREAM);
+        addEffectAction(EffectOptionFlags.NO_FALL_DAMAGE, 2, 1, Material.NETHERITE_BOOTS);
+        addEffectAction(EffectOptionFlags.SPEED, 4, 1, Material.FEATHER);
+        addEffectAction(EffectOptionFlags.NO_DURABILITY, 6, 1, Material.NETHERITE_PICKAXE);
+        addAction(
+                new MenuItem(
+                        44,
+                        Material.DIAMOND,
+                        "" + ChatColor.AQUA + ChatColor.BOLD + BingoTranslation.MENU_SAVE_EXIT.translate()
+                ),
+                (player) -> {
                     settings.effects(flags, session);
-                    close(player);
-                } else {
-                    toggleOption(menuItem);
                 }
-                return;
-            }
-        }
+        );
     }
 
-    private void toggleOption(EffectOptionsMenuItem menuItem) {
-        EffectOptionFlags flag = menuItem.getFlag();
+    private void addEffectAction(EffectOptionFlags flag, int slotX, int slotY, Material material) {
+        MenuItem item = new MenuItem(slotX, slotY, material, "");
+        addAction(
+                item,
+                (player) -> {
+                    toggleOption(flag);
+                    updateUI(flag, item);
+                },
+                false
+        );
+        updateUI(flag, item);
+    }
+
+    private void toggleOption(EffectOptionFlags flag) {
         if (flags.contains(flag))
             flags.remove(flag);
         else
             flags.add(flag);
-        updateUI(menuItem);
     }
 
-    public void updateUI(EffectOptionsMenuItem effectOptionsMenuItem) {
-        EffectOptionFlags flag = effectOptionsMenuItem.getFlag();
-        if (flag == null) {
-            return;
-        }
-        MenuItem menuItem = effectOptionsMenuItem.getMenuItem();
+    public void updateUI(EffectOptionFlags flag, MenuItem menuItem) {
         ItemMeta meta = menuItem.getItemMeta();
         if (meta != null) {
             if (flags.contains(flag)) {
