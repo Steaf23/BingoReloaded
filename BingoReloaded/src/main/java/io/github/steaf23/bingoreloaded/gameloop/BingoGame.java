c2cffcae53651d044692914ab655613cec92e3ca
chrisjanusa <chrisjanusa@gmail.com>
2023-06-18 10:51:44 -0700

    Fix stat config usage

--- BingoReloaded/src/main/java/io/github/steaf23/bingoreloaded/gameloop/BingoGame.java
+++ BingoReloaded/src/main/java/io/github/steaf23/bingoreloaded/gameloop/BingoGame.java
@@ -68,7 +68,7 @@ public class BingoGame implements GamePhase
         this.settings = settings;
         this.deadPlayers = new HashMap<>();
         this.cardEventManager = new CardEventManager(worldName);
-        if (config.disableStatistics)
+        if (!config.disableStatistics)
             this.statTracker = new StatisticTracker(worldName);
         else
             this.statTracker = null;
